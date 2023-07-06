6c01255bcb9b4f8e2d61747d29d3041fbf7dfbca
Leo <sliu9412@conestogac.on.ca~>
2023-07-06 01:28:17 -0400

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import HeaderNavigator from "./Components/HeaderNavigator";
 import "./App.scss";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
 
 function App() {
   return (
     <div>
       <HeaderNavigator></HeaderNavigator>
-      <main></main>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Home" element={<HomePage />} />
+          <Route path="/About" element={<AboutPage />} />
+        </Routes>
+      </main>
       <footer></footer>
     </div>
   );
